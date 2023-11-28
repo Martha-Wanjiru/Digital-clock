6af7ed3a5f6e914b7365b0171adb43a792ade98f
Martha_Wanjiru <marthaw.ict2001@gmail.com>
2023-11-28 15:41:55 +0300

    deploy

---
+++ main .js
@@ -0,0 +1,13 @@
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById('time').textContent = timeString;
+}
+
+setInterval(updateTime, 1000); // Update time every second
+updateTime(); // Initial call to set the time
+
